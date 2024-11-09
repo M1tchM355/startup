c65f57f9128db6307f48fe1fafc1f524a9e49c2c
M1tchM355 <mitchell.messerly@gmail.com>
2024-11-08 21:23:45 -0500

    changed login.jsx to match login.html

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,8 +2,23 @@ import React from 'react';
 
 export function Login() {
   return (
-    <main className='container-fluid text-center'>
-      <div>login displayed here</div>
+    <main className="container-fluid text-center">
+      <div>
+        <h1 className="intro">Welcome to RecipeShare!</h1>
+        <h4>Log in or create an account to get started:</h4>
+        <form method="get" action="new.html">
+          <div className="input-group mb-3">
+           <span className="input-group-text">@</span>
+           <input className="form-control" type="text" placeholder="your@email.com" />
+          </div>
+          <div className="input-group mb-3">
+           <span className="input-group-text">🔒</span>
+           <input className="form-control" type="password" placeholder="password" />
+          </div>
+          <button type="submit">Login</button>
+         <button type="submit">Create</button>
+        </form>
+      </div>
     </main>
   );
 }
