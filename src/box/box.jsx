423a5effeeda96fdea384bed344bc976e1ff1e19
M1tchM355 <mitchell.messerly@gmail.com>
2024-11-08 22:06:36 -0500

    moved box.html, community.html, and recipe.html to their respective jsx files

--- src/box/box.jsx
+++ src/box/box.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import './box.css'
 
 export function Box() {
   return (
-    <main className='container-fluid text-center'>
-      <div>box displayed here</div>
+    <main>
+      <h1>My Recipe Box</h1>
+      <section className="recipe-card">
+        <h3>Recipe 1</h3>
+        <p>5 stars, 1 comment(s)</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum aliquam condimentum ligula vel auctor. Morbi sit amet neque hendrerit, posuere mi in, mollis leo. Aliquam tempor lacus ut dignissim lobortis...</p>
+        <a href="recipe.html">See full recipe</a>
+      </section>
+      <section className="recipe-card">
+        <h3>Recipe 2</h3>
+        <p>4 stars, 1 comment(s)</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum aliquam condimentum ligula vel auctor. Morbi sit amet neque hendrerit, posuere mi in, mollis leo. Aliquam tempor lacus ut dignissim lobortis...</p>
+        <a href="recipe.html">See full recipe</a>
+      </section>
+      <section className="recipe-card">
+        <h3>Recipe 3</h3>
+        <p>4 stars, 0 comment(s)</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum aliquam condimentum ligula vel auctor. Morbi sit amet neque hendrerit, posuere mi in, mollis leo. Aliquam tempor lacus ut dignissim lobortis...</p>        
+        <a href="recipe.html">See full recipe</a>
+      </section>
+      <section className="recipe-card">
+        <h3>Recipe 4</h3>
+        <p>3 stars, 0 comment(s)</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum aliquam condimentum ligula vel auctor. Morbi sit amet neque hendrerit, posuere mi in, mollis leo. Aliquam tempor lacus ut dignissim lobortis...</p>        
+        <a href="recipe.html">See full recipe</a>
+      </section>
     </main>
   );
 }
--- src/community/community.jsx
+++ src/community/community.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
+import '../box/box.css'
 
 export function Community() {
   return (
-    <main className='container-fluid text-center'>
-      <div>community displayed here</div>
+    <main>
+      <h1>Community Recipe Box</h1>
+      <section className="recipe-card">
+        <h3>Recipe 1</h3>
+        <p>5 stars, 1 comment(s)</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum aliquam condimentum ligula vel auctor. Morbi sit amet neque hendrerit, posuere mi in, mollis leo. Aliquam tempor lacus ut dignissim lobortis...</p>
+        <a href="recipe.html">See full recipe</a>
+      </section>
+      <section className="recipe-card">
+        <h3>Recipe 2</h3>
+        <p>4 stars, 1 comment(s)</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum aliquam condimentum ligula vel auctor. Morbi sit amet neque hendrerit, posuere mi in, mollis leo. Aliquam tempor lacus ut dignissim lobortis...</p>
+        <a href="recipe.html">See full recipe</a>
+      </section>
+      <section className="recipe-card">
+        <h3>Recipe 3</h3>
+        <p>4 stars, 0 comment(s)</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum aliquam condimentum ligula vel auctor. Morbi sit amet neque hendrerit, posuere mi in, mollis leo. Aliquam tempor lacus ut dignissim lobortis...</p>
+        <a href="recipe.html">See full recipe</a>
+      </section>
+      <section className="recipe-card">
+        <h3>Recipe 4</h3>
+        <p>3 stars, 0 comment(s)</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum aliquam condimentum ligula vel auctor. Morbi sit amet neque hendrerit, posuere mi in, mollis leo. Aliquam tempor lacus ut dignissim lobortis...</p>
+        <a href="recipe.html">See full recipe</a>
+      </section>
+      <section className="recipe-card">
+        <h3>Recipe 5</h3>
+        <p>2 stars, 0 comment(s)</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum aliquam condimentum ligula vel auctor. Morbi sit amet neque hendrerit, posuere mi in, mollis leo. Aliquam tempor lacus ut dignissim lobortis...</p>
+        <a href="recipe.html">See full recipe</a>
+      </section>
+      <section className="recipe-card">
+        <h3>Recipe 6</h3>
+        <p>1 star, 0 comment(s)</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum aliquam condimentum ligula vel auctor. Morbi sit amet neque hendrerit, posuere mi in, mollis leo. Aliquam tempor lacus ut dignissim lobortis...</p>
+        <a href="recipe.html">See full recipe</a>
+      </section>
     </main>
   );
 }
