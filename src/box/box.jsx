e7cacdaae6807a887693963ab3c0f83234f1ca88
M1tchM355 <mitchell.messerly@gmail.com>
2024-11-08 22:17:49 -0500

    made routes to recipe.jsx on box.jsx and community.jsx

--- src/box/box.jsx
+++ src/box/box.jsx
@@ -16,19 +16,19 @@ export function Box() {
         <h3>Recipe 2</h3>
         <p>4 stars, 1 comment(s)</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum aliquam condimentum ligula vel auctor. Morbi sit amet neque hendrerit, posuere mi in, mollis leo. Aliquam tempor lacus ut dignissim lobortis...</p>
-        <a href="recipe.html">See full recipe</a>
+        <NavLink to="recipe">See full recipe</NavLink>
       </section>
       <section className="recipe-card">
         <h3>Recipe 3</h3>
         <p>4 stars, 0 comment(s)</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum aliquam condimentum ligula vel auctor. Morbi sit amet neque hendrerit, posuere mi in, mollis leo. Aliquam tempor lacus ut dignissim lobortis...</p>        
-        <a href="recipe.html">See full recipe</a>
+        <NavLink to="recipe">See full recipe</NavLink>
       </section>
       <section className="recipe-card">
         <h3>Recipe 4</h3>
         <p>3 stars, 0 comment(s)</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum aliquam condimentum ligula vel auctor. Morbi sit amet neque hendrerit, posuere mi in, mollis leo. Aliquam tempor lacus ut dignissim lobortis...</p>        
-        <a href="recipe.html">See full recipe</a>
+        <NavLink to="recipe">See full recipe</NavLink>
       </section>
     </main>
   );
--- src/community/community.jsx
+++ src/community/community.jsx
@@ -9,37 +9,37 @@ export function Community() {
         <h3>Recipe 1</h3>
         <p>5 stars, 1 comment(s)</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum aliquam condimentum ligula vel auctor. Morbi sit amet neque hendrerit, posuere mi in, mollis leo. Aliquam tempor lacus ut dignissim lobortis...</p>
-        <a href="recipe.html">See full recipe</a>
+        <NavLink to="recipe">See full recipe</NavLink>
       </section>
       <section className="recipe-card">
         <h3>Recipe 2</h3>
         <p>4 stars, 1 comment(s)</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum aliquam condimentum ligula vel auctor. Morbi sit amet neque hendrerit, posuere mi in, mollis leo. Aliquam tempor lacus ut dignissim lobortis...</p>
-        <a href="recipe.html">See full recipe</a>
+        <NavLink to="recipe">See full recipe</NavLink>
       </section>
       <section className="recipe-card">
         <h3>Recipe 3</h3>
         <p>4 stars, 0 comment(s)</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum aliquam condimentum ligula vel auctor. Morbi sit amet neque hendrerit, posuere mi in, mollis leo. Aliquam tempor lacus ut dignissim lobortis...</p>
-        <a href="recipe.html">See full recipe</a>
+        <NavLink to="recipe">See full recipe</NavLink>
       </section>
       <section className="recipe-card">
         <h3>Recipe 4</h3>
         <p>3 stars, 0 comment(s)</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum aliquam condimentum ligula vel auctor. Morbi sit amet neque hendrerit, posuere mi in, mollis leo. Aliquam tempor lacus ut dignissim lobortis...</p>
-        <a href="recipe.html">See full recipe</a>
+        <NavLink to="recipe">See full recipe</NavLink>
       </section>
       <section className="recipe-card">
         <h3>Recipe 5</h3>
         <p>2 stars, 0 comment(s)</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum aliquam condimentum ligula vel auctor. Morbi sit amet neque hendrerit, posuere mi in, mollis leo. Aliquam tempor lacus ut dignissim lobortis...</p>
-        <a href="recipe.html">See full recipe</a>
+        <NavLink to="recipe">See full recipe</NavLink>
       </section>
       <section className="recipe-card">
         <h3>Recipe 6</h3>
         <p>1 star, 0 comment(s)</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum aliquam condimentum ligula vel auctor. Morbi sit amet neque hendrerit, posuere mi in, mollis leo. Aliquam tempor lacus ut dignissim lobortis...</p>
-        <a href="recipe.html">See full recipe</a>
+        <NavLink to="recipe">See full recipe</NavLink>
       </section>
     </main>
   );
