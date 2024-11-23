75b183d33bb9df50f619b776cfea508ada38b19c
M1tchM355 <mitchell.messerly@gmail.com>
2024-11-22 20:25:44 -0500

    separated personal recipe box and community box in database.js

--- service/database.js
+++ service/database.js
@@ -5,9 +5,9 @@ const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db('simon');
+const db = client.db('startup');
 const userCollection = db.collection('user');
-const scoreCollection = db.collection('score');
+const recipeCollection = db.collection('communityRecipes');
 
 // This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
@@ -34,23 +34,32 @@ async function createUser(email, password) {
     email: email,
     password: passwordHash,
     token: uuid.v4(),
+    recipes: [],
   };
   await userCollection.insertOne(user);
 
   return user;
 }
 
-async function addScore(score) {
-  return scoreCollection.insertOne(score);
+async function addPersonalRecipe(recipe) {
+  //do this
 }
 
-function getHighScores() {
-  const query = { score: { $gt: 0, $lt: 900 } };
+async function addCommunityRecipe(recipe) {
+  return recipeCollection.insertOne(recipe);
+}
+
+function getPersonalRecipes() {
+  //to this
+}
+
+function getCommunityRecipes() {
+  const query = { //come back to this };
   const options = {
     sort: { score: -1 },
     limit: 10,
   };
-  const cursor = scoreCollection.find(query, options);
+  const cursor = recipeCollection.find(query, options);
   return cursor.toArray();
 }
 
@@ -58,6 +67,8 @@ module.exports = {
   getUser,
   getUserByToken,
   createUser,
-  addScore,
-  getHighScores,
+  addPersonalRecipe,
+  addCommunityRecipe,
+  getPersonalRecipes,
+  getCommunityRecipes
 };
--- service/index.js
+++ service/index.js
@@ -1,9 +1,10 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 const express = require('express');
-const uuid = require('uuid');
 const app = express();
+const DB = require('./database.js');
 
-let users = {};
-let communityBox = [];
+const authCookieName = 'token';
 
 // The service port. In production the front-end code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
@@ -11,25 +12,34 @@ const port = process.argv.length > 2 ? process.argv[2] : 3000;
 // JSON body parsing using built-in middleware
 app.use(express.json());
 
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
+
 // Serve up the front-end static content hosting
 app.use(express.static('public'));
+
+// Trust headers that are forwarded from the proxy so we can determine IP addresses
+app.set('trust proxy', true);
 
 // Router for service endpoints
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
-// CreateAuth a new user
+// CreateAuth token for a new user
 apiRouter.post('/auth/create', async (req, res) => {
-    const user = users[req.body.userName];
-    if (user) {
-        res.status(409).send({ msg: 'Existing user' });
+    if (await DB.getUser(req.body.email)) {
+      res.status(409).send({ msg: 'Existing user' });
     } else {
-        const user = { userName: req.body.userName, password: req.body.password, token: uuid.v4(), recipes: [] };
-        users[user.userName] = user;
-
-        res.send({ token: user.token });
+      const user = await DB.createUser(req.body.email, req.body.password);
+  
+      // Set the cookie
+      setAuthCookie(res, user.token);
+  
+      res.send({
+        id: user._id,
+      });
     }
-});
+  });
 
 // GetAuth login an existing user
 apiRouter.post('/auth/login', async (req, res) => {
